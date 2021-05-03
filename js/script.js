91969f46ff24c63677d7e2c4cc760c027291bad5
mattiadebonis <mattia.debonis@gmail.com>
2021-05-03 15:20:35 +0200

    Logica primo esercizio

--- js/script.js
+++ js/script.js
@@ -1,6 +1,23 @@
 // 1 - Palindroma
 // Chiedere all'utente di inserire una parola
+
 // Creare una funzione per capire se la parola inserita è palindroma
+
+// se il numeri dei caratteri è dispari la parola non è palindroma
+
+//altrimenti se è pari controlla se è palindroma
+
+//dividi a metà la parola
+
+//invertire ordine caratteri della seconda metà della parola
+
+//controllare se sono uguali
+
+
+
+
+
+
 // 2 - Pari e Dispari
 // L'utente sceglie pari o dispari e inserisce un numero da 1 a 5.
 // Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
